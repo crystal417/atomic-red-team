686df70db3af4d5c5e3051d8c46472f7b1c72948
crav3r <crav3r@gmail.com>
2021-06-25 11:49:11 +0900

    add t1562.002

--- round4test/Round4TestStep.ps1
+++ round4test/Round4TestStep.ps1
@@ -567,8 +567,30 @@ invoke-atomictest T1562.001 -testnumber 18 -Cleanup -ExecutionLogPath $Executio
 #------------------------------------------------------------
 # Defense Evasion - Impair Defenses(T1562) T1562.002 Disable Windows Event Logging
 
+#Atomic Test #1 - Disable Windows IIS HTTP Logging
+# 재테스트필요
+#invoke-atomictest T1562.002 -testnumber 1 -ExecutionLogPath $ExecutionLogPath
+#invoke-atomictest T1562.002 -testnumber 1 -Cleanup -ExecutionLogPath $ExecutionLogPath
+
+#Atomic Test #2 - Kill Event Log Service Threads
+#재태스트 필요
+#invoke-atomictest T1562.002 -testnumber 2 -ExecutionLogPath $ExecutionLogPath #관리자 권한
+#invoke-atomictest T1562.002 -testnumber 2 -Cleanup -ExecutionLogPath $ExecutionLogPath #관리자 권한
+
+#Atomic Test #3 - Impair Windows Audit Log Policy
+#재태스트 필요
+#invoke-atomictest T1562.002 -testnumber 3 -ExecutionLogPath $ExecutionLogPath
+#invoke-atomictest T1562.002 -testnumber 3 -Cleanup -ExecutionLogPath $ExecutionLogPath
+
+#Atomic Test #4 - Clear Windows Audit Policy Config
+invoke-atomictest T1562.002 -testnumber 4 -ExecutionLogPath $ExecutionLogPath
+invoke-atomictest T1562.002 -testnumber 4 -Cleanup -ExecutionLogPath $ExecutionLogPath
+
+
 #------------------------------------------------------------
 # Defense Evasion - Indicator Removal on Host(T1070) T1070.001 Clear Windows Event Logs
+
+
 
 #------------------------------------------------------------
 # Defense Evasion - Indicator Removal on Host(T1070) T1070.004 File Deletion
