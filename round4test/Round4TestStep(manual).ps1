e53d15d89f8cda3769baeb7462e533014f9d239b
sujeong1005 <sujeong.eun@ahnlab.com>
2021-06-29 14:39:53 +0900

    add manual test

---
+++ round4test/Round4TestStep(manual).ps1
@@ -0,0 +1,6 @@
+
+#------------------------------------------------------------
+#Impact - System Shutdown/Reboot(T1529) T1529 - System Shutdown/Reboot
+
+# Atomic Test #2 - Restart System - Windows
+invoke-atomictest T1529 -testnumber 2 -ExecutionLogPath $ExecutionLogPath #관리자 원한 #수동 실행
